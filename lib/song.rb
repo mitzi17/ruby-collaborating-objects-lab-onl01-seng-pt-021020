0ed36fd4faabc311f02fc6fa695b702e4956e136
Mitzi Estrada <m_estrada17@live.com>
2020-03-09 05:39:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,15 +21,9 @@ class Song
     artist, song = filename.split(" - ")
     new_song = self.new(song)
     new_song.artist_name = artist
-    new_song
+    new_song.save
   end
   
-  def self.new_by_filename(filename)
-    artist, song = filename.split(" - ")
-    new_song = self.new(song)
-    new_song.artist_name = artist
-    new_song
-  end
   
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
