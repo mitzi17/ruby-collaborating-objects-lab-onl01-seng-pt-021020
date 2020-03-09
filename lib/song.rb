d290927e3e00645fd2643fd58f0d96fe139dbcae
Mitzi Estrada <m_estrada17@live.com>
2020-03-09 05:18:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ class Song
   def new_by_filename(filename)
     artist, song = filename.split(" - ")
     new_song = self.new(name)
-    new_artist = self.new(name)
+    new_artist
     
     
   
