353847b226f07fa08a1308a002633d0b6ca2b2a5
Mitzi Estrada <m_estrada17@live.com>
2020-03-09 05:25:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,7 +20,9 @@ class Song
   
   def artist_name=(artist)
     self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song = self
+    artist.add_song(self)
+  end
+  
     
     
     
